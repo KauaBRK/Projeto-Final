2520bbb0a20dd2dd9142776c09b45a1ccf6e9b59
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-05-27 21:22:18 -0300

    Ajeitar isso aqui
    O loop repete uma vez antes de mostrar o resultado correto

--- Pagamento/cpf.c
+++ Pagamento/cpf.c
@@ -1,7 +1,26 @@
 void cpf(){
-	int cpf;
-	
+	char cpf[12];
 		system("cls");
 		printf("Digite o seu CPF:\n");
-		scanf("%d", &cpf);
+		scanf("%s", cpf);
+		
+					do {
+					printf("Digite o seu CPF:\n");
+					scanf("%s", cpf);
+						
+						if (strlen(cpf) < 11 || strlen(cpf) > 11){
+							printf("Por favor, digite 11 números.\n");
+							system("pause");
+							system("cls");
+							continue;
+						}
+						for(int i = 0; cpf[i] != '\0';i++){
+							if(!isdigit(cpf[i])){
+								printf("Por favor, digite apenas números\n\n");
+								system("pause");
+								break;
+							}
+						}
+					} while (strlen(cpf) < 11 || strlen(cpf) > 11);
 }
+
