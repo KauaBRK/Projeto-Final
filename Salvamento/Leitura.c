0851ebcdd788c7ffec0db932decd1cbb9a32abee
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2024-05-27 21:02:42 -0300

    Update Leitura.c

--- Salvamento/Leitura.c
+++ Salvamento/Leitura.c
@@ -4,12 +4,27 @@
 void leitura(int choice){
 	char text[500];
 	
-	FILE * file = fopen("Produtos.txt", "r");
-	if(file==NULL){
-			printf("Erro ao abrir o arquivo\nDica: crie um arquivo por meio do cadastro.");
+		if(choice==1){//Lista de funcionarios
+		FILE * file = fopen("Funcionarios.txt", "r");
+		if(file==NULL)
+				printf("Erro ao abrir o arquivo\nDica: crie um arquivo por meio do cadastro.");
+		if(file){
+			if(choice==1){
+				while(!feof(file)){
+				if(fgets(text,500,file))	
+					printf("%s", text);
+				}
+			}
 		}
+		fclose(file);
+	}
+	
+	if(choice==2){//Lista de produtos
+		FILE * file = fopen("Produtos.txt", "r");
+	if(file==NULL)
+			printf("Erro ao abrir o arquivo\nDica: crie um arquivo por meio do cadastro.");
 	if(file){
-		if(choice==1){//Lista de produtos
+		if(choice==2){
 			while(!feof(file)){
 			if(fgets(text,500,file))	
 				printf("%s", text);
@@ -17,6 +32,7 @@ void leitura(int choice){
 		}
 	}
 	fclose(file);
+	}
 }
 int main(){
 	int choice=1;
