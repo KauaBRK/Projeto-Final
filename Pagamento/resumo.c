8fbdd6a7797d1fa05b58c1e22ae198683959b0ef
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-05-28 13:56:06 -0300

    Minor adjustments

--- Pagamento/resumo.c
+++ Pagamento/resumo.c
@@ -1,6 +1,6 @@
 void summary(float price, int op, char nomeProduto[]){
 	system("cls");
-	printf("Nome do produto: %s\nValor do produto: %.2f\n", nomeProduto, price);
+	printf("Nome do produto: %s\nValor do produto: R$%.2f\n", nomeProduto, price);
 	if(op == 1 || op == 2 || op == 3) 
 		printf("Método de pagamento: %dX de R$%.2f\n\n", op, price / op);
 	else if(op == 4)
