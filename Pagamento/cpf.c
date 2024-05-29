96c54262e42bfd2101b38a2b20493d7e662b492d
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-05-29 18:57:52 -0300

    Update cpf.c

--- Pagamento/cpf.c
+++ Pagamento/cpf.c
@@ -1,26 +1,24 @@
 void cpf(){
 	char cpf[12];
-		system("cls");
+	system("cls");
+		
+		do {
 		printf("Digite o seu CPF:\n");
 		scanf("%s", cpf);
-		
-					do {
-					printf("Digite o seu CPF:\n");
-					scanf("%s", cpf);
-						
-						if (strlen(cpf) < 11 || strlen(cpf) > 11){
-							printf("Por favor, digite 11 números.\n");
-							system("pause");
-							system("cls");
-							continue;
-						}
-						for(int i = 0; cpf[i] != '\0';i++){
-							if(!isdigit(cpf[i])){
-								printf("Por favor, digite apenas números\n\n");
-								system("pause");
-								break;
-							}
-						}
-					} while (strlen(cpf) < 11 || strlen(cpf) > 11);
+			
+			if (strlen(cpf) < 11 || strlen(cpf) > 11){
+				printf("Por favor, digite apenas 11 números.\n");
+				system("pause");
+				system("cls");
+				continue;
+			} 			
+			for(int i = 0; cpf[i] != '\0';i++){
+					if (!isdigit(cpf[i])){
+					printf("Por favor, digite apenas números\n\n");
+					cpf[i] = '\0';
+					system("pause");
+					system("cls");
+				}
+			}
+		} while (strlen(cpf) < 11 || strlen(cpf) > 11 || cpf != '\0');
 }
-
